2942581a764cd3469cecc74e87a41b5c45460745
Michael Wales <michaelwales@gmail.com>
2020-03-16 18:06:40 +0000

    Sets up class and getter methods in Student file

--- code_clan_student.rb
+++ code_clan_student.rb
@@ -1,3 +1,16 @@
-def student_name()
-  return @student_name
+class Student
+
+  def initialize(student_name, cohort)
+    @student_name = student_name
+    @cohort = cohort
+  end
+
+  def student_name()
+    return @student_name
+  end
+
+  def cohort()
+    return @cohort
+  end
+
 end
--- specs/code_clan_student_specs.rb
+++ specs/code_clan_student_specs.rb
@@ -1,14 +1,12 @@
 require('minitest/autorun')
 require('minitest/reporters')
-require_relative('../homework')
+require_relative('../code_clan_student')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new()
 
-class Student < MiniTest::Test
+class TestStudent < MiniTest::Test
 
   def setup
     @student = Student.new("Michael", "G18")
   end
-
-
-
+  
 end
